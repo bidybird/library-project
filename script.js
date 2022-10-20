af9573d0f2aa905033867a808a59390f8a0de47d
Jonathan Shovak <shovakitup@gmail.com>
2022-10-20 15:06:32 -0500

    added four example book objects, book info is not accurate

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 let myLibrary = [];
 
-function Book(title, author, page, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.page = page;
+  this.pages = pages;
   this.read = read;
 }
 
 function addBookToLibrary() {
   //do stuff here
 }
+
+const theHobbit = new Book("The Hobbit", "Tolkien", "285", "read");
+
+const theLordOfTheRings = new Book(
+  "The Lord of the Rings",
+  "Tolkien",
+  "325",
+  "not read yet"
+);
+
+const sutree = new Book("Sutree", "Mr. Guy", "200", "not read yet");
+
+const brothersOfBlood = new Book(
+  "Brothers of Blood",
+  "Tristan",
+  "165",
+  "not read yet"
+);
